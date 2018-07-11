eaa17710e305cc1f501a1f60b58ac6ab82b8069f
FrancoPandolfo <39636669+FrancoPandolfo@users.noreply.github.com>
2018-07-11 15:14:19 -0300

    grafo

---
+++ grafo.py
@@ -0,0 +1,36 @@
+
+class grafo:
+	def __init__(self,dic):
+		self.dic = dic
+
+	def AgregarVertice(self,vertice):
+		self.dic[vertice] = {}
+
+	def AgregarArista(self,arista):
+		self.dic[arista[0]][arista[1]] = ""
+		self.dic[arista[1]][arista[0]] = ""
+
+	def SacarVertice(self,vertice):
+		del self.dic[vertice]
+		l1=list(self.dic.keys())
+		for v in l1:
+			l2=list(self.dic[v].keys())
+			for i in l2:
+				if i==vertice:
+					del self.dic[v][i]
+
+	def SacarArista(self,arista):
+		del self.dic[arista[0]][arista[1]]
+		del self.dic[arista[1]][arista[0]]
+
+	def EstanConectados(self,v1,v2):
+		if v2 in self.dic[v1]:
+			return True
+		else:
+			return False
+
+	def VerVecinos(self,vertice):
+		list = []
+		for v in self.dic[vertice]:
+			list.append(v)
+		return list

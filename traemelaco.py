e17e10b4a0d7993931cd63f37ec5630f7b590a25
FrancoPandolfo <39636669+FrancoPandolfo@users.noreply.github.com>
2018-07-16 02:39:12 -0300

    solucionado el error de key cuando se ingresa por ejemeplo San Petesburgo como parametro, antes leia san, ahora lo lee entero

--- traemelaco.py
+++ traemelaco.py
@@ -39,15 +39,37 @@ else:
 	sys.exit()
 
 while True:
-	
-	text = input("entrada:")
-	if len(text) > 1:
-		text[-1]
-	cadena = text.split(' ')
-	print(text)
+	try:
+  		text = input("entrada:")
+  		cadena = text.split(' ')
+	except (EOFError):
+		break		
 
 	if cadena[0] == 'ir':
-		cadena[1] = cadena[1][:-1]
+		pos_coma = 0
+		for i in range(1,len(cadena)):
+			for j in range(0,len(cadena[i])):
+				if cadena[i][j] == ',':
+					pos_coma = i
+					break
+		cadena[pos_coma] = cadena[pos_coma][:-1]
+		s1 = ' '
+		actual1 = []
+		if i > 1:
+			for i in range(1,pos_coma+1):
+				actual1.append(cadena[i])
+			cadena[1] = s1.join(actual1)
+
+		cont = 0
+		for i in range(pos_coma+1,len(cadena)): 
+			cont += 1
+		s2 = ' '
+		actual2 = []
+		if cont > 1:
+			for i in range(pos_coma+1,len(cadena)):
+				actual2.append(cadena[i])
+			cadena[2] = s2.join(actual2)
+
 		camino = lib.camino_minimo(grafoPesado,cadena[1],cadena[2])
 		for i in range(0,len(camino[0])-1):
 			print(camino[0][i],'-> ', end='')
@@ -58,6 +80,12 @@ while True:
 
 	elif cadena[0] == 'viaje':
 		cadena[1] = cadena[1][:-1]
+		s = ' '
+		actual = []
+		if len(cadena) > 3:
+			for i in range(2,len(cadena)):
+				actual.append(cadena[i])
+			cadena[2] = s.join(actual)
 
 		if cadena[1] == 'optimo':
 			camino = lib.viajante(grafoPesado,cadena[2])
